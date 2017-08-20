ceebdc3ccbacecf8829912a9eccb38745091ed8a
Lucas Barboza <lucas.barboza@sprinklr.com>
2017-08-20 15:25:43 -0300

    Fix lint

--- nodejs/Infra/Migrations/1-create-collection.js
+++ nodejs/Infra/Migrations/1-create-collection.js
@@ -1,10 +1,10 @@
-const { messages } = require("./Seed/messages_sample")
+const { messages } = require('./Seed/messages_sample')
 
-module.exports.id = "create-collection"
+module.exports.id = 'create-collection'
 
 module.exports.up = function (done) {
-  this.db.createCollection('messages', {}, function(error, collection) {
-    collection.insertMany(messages, function(error, response){
+  this.db.createCollection('messages', {}, function (_, collection) {
+    collection.insertMany(messages, function (_, response) {
       done()
     })
   })
