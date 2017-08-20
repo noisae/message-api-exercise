5f51afde9f0c36ff3ba14c6063270b8d5ae7b2d1
Lucas Barboza <lucas.barboza@sprinklr.com>
2017-08-20 17:54:15 -0300

    Fix pagination

--- nodejs/Application/route/message/Message.controller.js
+++ nodejs/Application/route/message/Message.controller.js
@@ -6,37 +6,37 @@ const messageController = {
   list (request, injection) {
     const { ApiService } = Object.assign({}, dependencies, injection)
 
-    const page = request.query.page > 0 && request.query.page > 0 && request.query.page || 1
-    const limit = request.query.limit > 0 && request.query.limit > 0 && request.query.limit || 10
+    const page = request.query.page > 0 && parseInt(request.query.page) || 1
+    const limit = request.query.limit > 0 && parseInt(request.query.limit) || 10
 
     return ApiService.list({ page, limit }, limit)
   },
   listArchived (request, injection) {
     const { ApiService } = Object.assign({}, dependencies, injection)
 
-    const page = request.query.page > 0 && request.query.page > 0 && request.query.page || 1
-    const limit = request.query.limit > 0 && request.query.limit > 0 && request.query.limit || 10
+    const page = request.query.page > 0 && parseInt(request.query.page) || 1
+    const limit = request.query.limit > 0 && parseInt(request.query.limit) || 10
 
     return ApiService.listArchived({ page, limit }, limit)
   },
   show (request, injection) {
     const { ApiService } = Object.assign({}, dependencies, injection)
 
-    const uid = request.params.uid > 0 && request.params.uid > 0 && request.params.uid || 1
+    const uid = request.params.uid > 0 && request.params.uid || 1
 
     return ApiService.show(uid)
   },
   read (request, injection) {
     const { ApiService } = Object.assign({}, dependencies, injection)
 
-    const uid = request.params.uid > 0 && request.params.uid > 0 && request.params.uid || 1
+    const uid = request.params.uid > 0 && request.params.uid || 1
 
     return ApiService.read(uid)
   },
   archive (request, injection) {
     const { ApiService } = Object.assign({}, dependencies, injection)
 
-    const uid = request.params.uid > 0 && request.params.uid > 0 && request.params.uid || 1
+    const uid = request.params.uid > 0 && request.params.uid || 1
 
     return ApiService.archive(uid)
   }
